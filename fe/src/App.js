17af49269a8f051dfc3d9c2e4e986f3352b3a4c8
이제원 <lys28750@naver.com>
2024-08-24 01:31:03 +0900

    resultPage continue

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,10 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
+import { SearchResults } from './pages/SearchResults';
 
 function App() {
   return (
     <>
       <Routes>
-        
+        <Route path="/searchresult" element={<SearchResults/>}/>
       </Routes>
     </>
   );
